6bb412d44e5e7dd684ddb9fe0ba9878f74e9c34e
Romy Ilano <romy@snowyla.com>
2013-02-03 16:39:16 -0800

    Initial Commit

---
+++ Question and Answer/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Question and Answer
+//
+//  Created by Romy Ilano on 2/3/13.
+//  Copyright (c) 2013 Romy Ilano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Question and Answer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Question and Answer
+//
+//  Created by Romy Ilano on 2/3/13.
+//  Copyright (c) 2013 Romy Ilano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
